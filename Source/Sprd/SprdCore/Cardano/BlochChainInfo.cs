8ecc3d76d6182a57c5e68fed6544ac3ae31a999a
Patrick <gruberpatrickit@gmail.com>
2021-04-19 22:57:52 +0200

    updated blockchain network params

--- Source/Sprd/SprdCore/Cardano/BlochChainInfo.cs
+++ Source/Sprd/SprdCore/Cardano/BlochChainInfo.cs
@@ -3,11 +3,11 @@
     public static class BlochChainInfo
     {
         public static int LovelaceToAda = 1000000;
-        public static long TotalSupplyAda = 31100000000; // Updated 13.03.2021 from AdaPools.org  ToDo: should be replaced with cardano-db-sync
-        public static long TotalStakedAda = 22790000000; // Updated 13.03.2021 from AdaPools.org  ToDo: should be replaced with cardano-db-sync
-        public static double DecentralacationFactor = 0.10; // Updated 13.03.2021 from AdaPools.org  ToDo: should be replaced with cardano-db-sync
-        public static int BlocksPerEpoch = 21000; // Updated 13.03.2021 from AdaPools.org  ToDo: should be replaced with cardano-db-sync
-        public static int KValue = 500; // Updated 13.03.2021 from AdaPools.org  ToDo: should be replaced with cardano-db-sync
+        public static long TotalSupplyAda = 32100000000; // Updated 19.04.2021 from AdaPools.org  ToDo: should be replaced with cardano-db-sync
+        public static long TotalStakedAda = 22710000000; // Updated 19.04.2021 from AdaPools.org  ToDo: should be replaced with cardano-db-sync
+        public static double DecentralacationFactor = 0.02; // Updated 19.04.2021 from AdaPools.org  ToDo: should be replaced with cardano-db-sync
+        public static int BlocksPerEpoch = 21000; // Updated 19.04.2021 from AdaPools.org  ToDo: should be replaced with cardano-db-sync
+        public static int KValue = 500; // Updated 19.04.2021 from AdaPools.org  ToDo: should be replaced with cardano-db-sync
 
         public static double GetBlockChance(long activeStake)
         {
